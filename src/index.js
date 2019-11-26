77966d911d00d4030313870eda74a6d0334a7c2f
Pavel Angelov <pavel@st6.io>
2019-11-26 10:50:04 +0200

    Move react frontend to gui dir

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './gui/index.css';
+import App from './gui/App';
+import * as serviceWorker from './gui/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
